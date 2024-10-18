dc7953f75fe9077d6308a0f51b731229bfa67a08
jonathanforhan <jonathan.forhan@gmail.com>
2024-10-18 19:47:52 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <lua.hpp>
+
+lua_State* L = luaL_newstate();
+
+int main() {
+    luaL_openlibs(L);
+
+    const char* s = "a = 9 + 10 + math.cos(45*math.pi/180)";
+
+    int r = luaL_dostring(L, s);
+
+    if (r == LUA_OK) {
+        lua_getglobal(L, "a");
+
+        if (lua_isnumber(L, -1)) {
+            float float32 = (float)lua_tonumber(L, -1);
+            printf("result: %f\n", float32);
+        }
+    } else {
+        const char* error_msg = lua_tostring(L, -1);
+        printf("%s\n", error_msg);
+    }
+
+    lua_close(L);
+    return 0;
+}
---
+++ tests/ohms_law/ckt.lua
@@ -0,0 +1,10 @@
+-- edu-spice ohms_law circuit netlist description
+
+return {
+    V = {
+        { from = 0, to = 1, dc = 5 }
+    },
+    R = {
+        { from = 0, to = 1, r = 10e3 }
+    }
+};
